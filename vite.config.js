52858b3769cb30108b5bda3d964f943fa87a61f6
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2022-06-28 19:15:05 +0200

    chore: configure svelte config module, vite only with the plugin

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
 import { sveltekit } from '@sveltejs/kit/experimental/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-    plugins: [sveltekit({
-        // Consult https://github.com/sveltejs/svelte-preprocess
-        // for more information about preprocessors
-        preprocess: preprocess(),
-        adapter: adapter(),
-
-        // Override http methods in the Todo forms
-        methodOverride: {
-            allowed: ['PATCH', 'DELETE']
-        }
-    })]
+    plugins: [sveltekit()]
 };
 
 export default config;
