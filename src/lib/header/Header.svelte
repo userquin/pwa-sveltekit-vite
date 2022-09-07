0aa4ce604a8e7872581447955ec5054563f98c1c
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-09-07 08:58:47 -0700

    Upgrade to the latest SvelteKit (#2)

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -17,9 +17,9 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
+			<li class:active={$page.url.pathname === '/'}><a data-sveltekit-prefetch href="/">Home</a></li>
 			<li class:active={$page.url.pathname === '/about'}>
-				<a sveltekit:prefetch href="/about">About</a>
+				<a data-sveltekit-prefetch href="/about">About</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
